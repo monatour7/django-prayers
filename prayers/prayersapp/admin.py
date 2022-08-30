d6b58570b2bae461dc76cf0aef7a3358592ed1f4
HusseinBarham-CS <mohammed.natour7@gmail.com>
2022-08-30 18:56:34 -0400

    Customizing the admin

--- prayers/prayersapp/admin.py
+++ prayers/prayersapp/admin.py
@@ -2,4 +2,6 @@ from django.contrib import admin
 
 from prayers.prayersapp.models import PrayersTime
 
-admin.site.register(PrayersTime)
+@admin.register(PrayersTime)
+class PersonAdmin(admin.ModelAdmin):
+    pass
